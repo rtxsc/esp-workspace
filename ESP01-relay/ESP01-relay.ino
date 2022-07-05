b1c12c46a16eb61963933d5d4793e7ddf384b81f
rtxsc <robotronix.uitm@gmail.com>
2022-07-05 20:44:36 +0800

    update

--- ESP01-relay/ESP01-relay.ino
+++ ESP01-relay/ESP01-relay.ino
@@ -1,13 +1,28 @@
+#define SMART_EXTENSION
+
 #define BLYNK_PRINT Serial
+
+#ifndef SMART_EXTENSION
 #define BLYNK_TEMPLATE_ID "TMPLkJW7Yrd6"
 #define BLYNK_DEVICE_NAME "Home Automation"
 #define BLYNK_AUTH_TOKEN "zwgdxF7RjqtaRJf8akVfWE4cEXiRFEvd"
+#else
+#define BLYNK_TEMPLATE_ID "TMPLkJW7Yrd6"
+#define BLYNK_DEVICE_NAME "Smart Extension Cord"
+#define BLYNK_AUTH_TOKEN "pGraePxIauIZhHislUGs8JCn875bjP6I"
+#endif
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
 #include "uptime_formatter.h"
 #include <NTPClient.h>
 #include <WiFiUdp.h>
+#include "EEPROM.h"
+
+
+#define EEPROM_SIZE 5
+#define restartCounterAddress   0x00 // 15 : 1 byte
+
 
 #include <Pinger.h>
 extern "C"
@@ -16,14 +31,16 @@ extern "C"
 }
 
 char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "NPRDC CELCOM M1"; // iPhone 12 Pro Max
+char pass[] = "nprdc1234";
 // char ssid[] = "iPhone 12 Pro Max"; // iPhone 12 Pro Max
 // char pass[] = "robotronix";
-char ssid[] = "MaxisONE Fibre 2.4G"; // iPhone 12 Pro Max
-char pass[] = "respironics";
+// char ssid[] = "MaxisONE Fibre 2.4G"; // iPhone 12 Pro Max
+// char pass[] = "respironics";
 const char* remote_host = "blynk.cloud";
 
-#define LED_BUILTIN2 2
-#define RELAY 0
+#define LED_BUILTIN2  2
+#define RELAY         0
 
 WidgetLED relayStateLight(V20);
 WidgetLED ledStateLight(V21);
@@ -39,13 +56,14 @@ bool        automatic = false;
 bool        relayAlreadyOn = false;
 long        started ;
 long        onElapse ;
+
 BLYNK_CONNECTED() {
-  Blynk.syncVirtual(V0, V1);
+  Blynk.syncVirtual(V0, V1, V2);
 }
 
 void get_ping(){
 
-  Serial.printf("\n\nPinging Blynk.Cloud\n");
+  // Serial.printf("\n\nPinging Blynk.Cloud\n");
   if(pinger.Ping("blynk.cloud") == false)
   {
     Serial.println("Error during ping command.");
@@ -55,12 +73,12 @@ void get_ping(){
   {
     if (response.ReceivedResponse)
     {
-      Serial.printf(
-        "Reply from %s: bytes=%d time=%lums TTL=%d\n",
-        response.DestIPAddress.toString().c_str(),
-        response.EchoMessageSize - sizeof(struct icmp_echo_hdr),
-        response.ResponseTime,
-        response.TimeToLive);
+      // Serial.printf(
+      //   "Reply from %s: bytes=%d time=%lums TTL=%d\n",
+      //   response.DestIPAddress.toString().c_str(),
+      //   response.EchoMessageSize - sizeof(struct icmp_echo_hdr),
+      //   response.ResponseTime,
+      //   response.TimeToLive);
     }
     else
     {
@@ -82,26 +100,26 @@ void get_ping(){
     }
     
     // Print packet trip data
-    Serial.printf(
-      "Ping statistics for %s:\n",
-      response.DestIPAddress.toString().c_str());
-    Serial.printf(
-      "    Packets: Sent = %lu, Received = %lu, Lost = %lu (%.2f%% loss),\n",
-      response.TotalSentRequests,
-      response.TotalReceivedResponses,
-      response.TotalSentRequests - response.TotalReceivedResponses,
-      loss);
+    // Serial.printf(
+    //   "Ping statistics for %s:\n",
+    //   response.DestIPAddress.toString().c_str());
+    // Serial.printf(
+    //   "    Packets: Sent = %lu, Received = %lu, Lost = %lu (%.2f%% loss),\n",
+    //   response.TotalSentRequests,
+    //   response.TotalReceivedResponses,
+    //   response.TotalSentRequests - response.TotalReceivedResponses,
+    //   loss);
 
     // Print time information
     if(response.TotalReceivedResponses > 0)
     {
-      Serial.printf("Approximate round trip times in milli-seconds:\n");
-      Serial.printf(
-        "    Minimum = %lums, Maximum = %lums, Average = %.2fms\n",
-        response.MinResponseTime,
-        response.MaxResponseTime,
-        response.AvgResponseTime);
-        Blynk.virtualWrite(V12, response.AvgResponseTime);
+      // Serial.printf("Approximate round trip times in milli-seconds:\n");
+      // Serial.printf(
+      //   "    Minimum = %lums, Maximum = %lums, Average = %.2fms\n",
+      //   response.MinResponseTime,
+      //   response.MaxResponseTime,
+      //   response.AvgResponseTime);
+      Blynk.virtualWrite(V12, response.AvgResponseTime);
 
     }
     /*
@@ -159,42 +177,57 @@ void get_uptime(){
   Blynk.virtualWrite(V10, uptime);
 }
 
-
-BLYNK_WRITE(V5)
-{
-  // You'll get HIGH/1 at startTime and LOW/0 at stopTime.
-  // this method will be triggered every day
-  // until you remove widget or stop project or
-  // clean stop/start fields of widget
-  int stateFromTimer = param.asInt(); // assigning incoming value from pin V1 to a variable
-
-  Serial.print("Got a value from Timer event: ");
-  Serial.println(stateFromTimer);
-  if(automatic){
-      if(stateFromTimer) Serial.println("Turning ON Relay and LED");
-      else Serial.println("Turning OFF Relay and LED");
-      digitalWrite(RELAY, !stateFromTimer);   
-      digitalWrite(LED_BUILTIN2, !stateFromTimer);   
-  }
-}
-
 BLYNK_WRITE(V0)
 {
   int pinValue = param.asInt(); 
   if(!automatic){
-      Serial.print("Value from manual button: ");
-      Serial.println(pinValue);
     digitalWrite(RELAY, !pinValue);   
-    digitalWrite(LED_BUILTIN2, !pinValue);
   }   
 }
 
 BLYNK_WRITE(V1)
 {
   automatic = param.asInt(); // assigning incoming value from pin V1 to a variable
-  Serial.print("automation: ");
-  Serial.println(automatic);
+  digitalWrite(LED_BUILTIN2, !automatic);
+
 }
+
+BLYNK_WRITE(V2){
+  int pinValue = param.asInt();
+  if(pinValue){
+    Serial.println("########## CLEARING MEMORY ##########");
+    for (int w=0; w < EEPROM_SIZE ; w++){
+      EEPROM.write(w, 0);
+    }
+    EEPROM.commit();
+    delay(5);
+    Serial.println("########## AUTO RESET ESP ##########");
+    ESP.restart();
+  }
+}
+
+byte check_restart_count(byte prevRC){
+  byte newRC = prevRC + 1;  // increment rst count by 1 for each reboot
+  EEPROM.write(restartCounterAddress, newRC);
+  EEPROM.commit();
+  delay(5);
+
+  return newRC;
+}
+
+
+void init_eeprom(){
+
+  EEPROM.begin(EEPROM_SIZE);
+  Serial.println(" bytes read from Flash:");
+  for (int i = 0; i < EEPROM_SIZE; i++)
+  {
+    Serial.print("R "); Serial.print(i);   Serial.print(":");
+    Serial.print(byte(EEPROM.read(i))); Serial.println();
+  }
+  Serial.println();
+}
+
 
 void printTimeNTP(){
   timeClient.update();
@@ -245,30 +278,27 @@ String getReadableTime(int motion_timeout_sec) {
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
-  Serial.begin(9600);
+  Serial.begin(115200);
+
+  init_eeprom();
+  byte prevRestart = EEPROM.read(restartCounterAddress);
+  byte newRestart = check_restart_count(prevRestart);
+
   pinMode(LED_BUILTIN2, OUTPUT);
   pinMode(RELAY, OUTPUT);
-  digitalWrite(RELAY, LOW);   
-  digitalWrite(LED_BUILTIN2, LOW);
+  digitalWrite(RELAY, HIGH);   
+  digitalWrite(LED_BUILTIN2, HIGH);
   delay(1000);
   
   Blynk.begin(auth, ssid, pass);
-  digitalWrite(RELAY, LOW);   
-  digitalWrite(LED_BUILTIN2, LOW);
+  digitalWrite(RELAY, HIGH);   
+  digitalWrite(LED_BUILTIN2, HIGH);
   delay(1000);
 
-   // Ping default gateway
-  Serial.printf(
-    "\n\nPinging default gateway with IP %s\n",
-    WiFi.gatewayIP().toString().c_str());
-  if(pinger.Ping(WiFi.gatewayIP()) == false)
-  {
-    Serial.println("Error during last ping command.");
-  }
-  delay(5000);
-
   Blynk.virtualWrite(V13,WiFi.localIP().toString());
-  
+  Blynk.virtualWrite(V15,ssid);
+  Blynk.virtualWrite(V16,newRestart);
+
   timeClient.begin();
   timeClient.setTimeOffset(28800);
 
