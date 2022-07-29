3195439d182616c48d117f0315b78cf3441b5b0d
rtxsc <robotronix.uitm@gmail.com>
2022-07-29 19:27:15 +0800

    Update interrupt_test.ino

--- interrupt_test/interrupt_test.ino
+++ interrupt_test/interrupt_test.ino
@@ -1,3 +1,4 @@
+#include <util/atomic.h> // this library includes the ATOMIC_BLOCK macro.
 #define INTERVAL_METHOD
  #define USE_TM1637 // comment this line if you dont have the display 
 #define ENABLE_SERIAL_DEBUG
@@ -8,7 +9,6 @@
 
 #define CLK                 8
 #define DIO                 9
-#define ELAPSE_ONE_SECOND   1000
 #define MINUTE_PER_HOUR     60
 #define TICKS_PER_MINUTE    60
 #define DIVISOR             1 // higher = faster update rate (need more magnets for better precision)
@@ -20,20 +20,30 @@
 TM1637Display display(CLK, DIO);
 #endif
 
+
+const int ELAPSE_ONE_SECOND = 1000;
 const byte ledPin = 13;
 const byte button = 2;
 const byte interruptPin = 3;
 volatile byte state = LOW;
-volatile uint32_t tick = 0;
-volatile uint32_t ticks_per_sec = 0;
+volatile uint16_t tick = 0;
+volatile uint16_t ticks_per_sec = 0;
 volatile uint8_t DIVISOR_VALUE = 1;
-uint8_t divi[] = { 0xff };
+
+#define M1A 5
+#define THROTTLE 0
+
+uint16_t min_read = 0;
+uint16_t max_read = 0;
+
+bool minimum_obtained = false;
+bool maximum_obtained = false;
 
 
 long startMillis;
 int elapse = 0;
-float speed_kmh = 0.0;
-float speed2_kmh = 0.0;
+double speed_kmh = 0.0;
+double speed2_kmh = 0.0;
 int rpm = 0;
 
 float r = 0.056; // 5.6 cm or 0.056m (fan) /// 3.3cm or 0.033m (HDD 2.5") // 352mm for SunDing
@@ -56,8 +66,8 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(button), get_button_press, FALLING);
   attachInterrupt(digitalPinToInterrupt(interruptPin), get_tick, FALLING);
   Serial.begin(115200);
+  for(int x = 0; x < 50; x++) Serial.println();
   Serial.println("Speedometer Test using Arduino");
-
   startMillis = millis(); // start ticker
 
   #ifdef USE_TM1637
@@ -74,24 +84,39 @@ void setup() {
   }
   #endif
 
+  pinMode(M1A, OUTPUT); // PWM
+  pinMode(THROTTLE, INPUT);
+   
+  Serial.println("Throttle Test & Encoder Speed Test");
+  delay(1000);
+  calibrate();
+
 }
 
 void loop() {
-
+    ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+  // code with interrupts blocked (consecutive atomic operations will not get interrupted)
   // digitalWrite(ledPin, state);
   // ELAPSE_ONE_SECOND/DIVISOR is the scaler for update rate
   // default to 1000 / 10 = 100 millisecond = 0.1 second
 
   #ifdef INTERVAL_METHOD
-
+  drive_motor();
   if(DIVISOR_VALUE > 10) DIVISOR_VALUE = 1;
-  divi[0] = display.encodeDigit(DIVISOR_VALUE);
-  // display.setSegments(divi);
+
+  // ticks_per_sec = (tick * DIVISOR_VALUE);
+  // Serial.print("Divisor:");
+  // Serial.print(DIVISOR_VALUE);
+  // Serial.print("\tTick:");
+  // Serial.print(tick);
+  // Serial.print("\tTick/sec:");
+  // Serial.println(ticks_per_sec);
+  // delay(1000);
 
   if(millis()-startMillis >= ELAPSE_ONE_SECOND/DIVISOR_VALUE){
+
     ticks_per_sec = (tick * DIVISOR_VALUE);
     dist += ticks_per_sec * circf / 1000; // distance in km
-
     rpm = (ticks_per_sec * TICKS_PER_MINUTE ) / MAGNET_COUNT; // 1 tick / sec =  60 ticks per minute
     speed_kmh = (rpm * circf * MINUTE_PER_HOUR) / 1000; ; 
 
@@ -141,6 +166,7 @@ void loop() {
 
     tick = 0; // reset tick to zero
     startMillis = millis(); // reset millis()
+
   }
   #else
   // ifdef ELAPSE_METHOD
@@ -190,13 +216,77 @@ void loop() {
 
   #endif
 
- }
+
+    } // ATOMIC END
+
+ } // END OF LOOP
 
 void get_tick() {
-//  state = !state;
   tick++;
 }
 
 void get_button_press(){
   DIVISOR_VALUE++;
+}
+
+void drive_motor(){
+  int read_throttle = analogRead(THROTTLE);
+  if(read_throttle < min_read)   min_read = read_throttle;
+  if(read_throttle > max_read)   max_read = read_throttle;
+  
+  int mapped_throttle = map(read_throttle, min_read, max_read, 0, 255);
+  analogWrite(M1A, mapped_throttle);
+  
+  // Serial.print("Raw:");
+  // Serial.print(read_throttle);
+  // Serial.print("\tMin:");
+  // Serial.print(min_read);
+  // Serial.print("\tMax:");
+  // Serial.print(max_read);
+  // Serial.print("\tThrottle:");
+  // Serial.println(mapped_throttle);
+
+}
+
+
+void calibrate(){
+   // put your main code here, to run repeatedly:
+  int read_throttle = analogRead(THROTTLE);
+  if(read_throttle < min_read)   min_read = read_throttle;
+  if(read_throttle > max_read)   max_read = read_throttle;
+
+  if(!minimum_obtained){
+    min_read = read_throttle;
+    minimum_obtained = true;
+    Serial.println("Minimum throttle obtained");
+    delay(100);
+  }
+
+  while(!maximum_obtained){
+    Serial.print("Push throttle to max in:");
+    for(int i=0; i<5; i++){
+      Serial.print(5-i);
+      Serial.print(" ");
+      delay(100);
+    }
+    Serial.println();
+    read_throttle = analogRead(THROTTLE);
+    if(read_throttle < min_read + 50){
+       Serial.println("Throttle not pushed to max! Please do so now!!!");
+       delay(100);
+    }
+    else{
+       max_read = read_throttle;
+      Serial.println("Maximum throttle obtained");
+      delay(100);
+      while(read_throttle > min_read + 50){
+        read_throttle = analogRead(THROTTLE); // to check if the throttle is held max
+        Serial.println("Release the throttle to begin...");
+        delay(100);
+      }
+      maximum_obtained = true;
+    }
+   delay(100);
+
+  }
 }
